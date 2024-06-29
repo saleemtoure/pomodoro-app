b9c73ede843930b5fa73bea90adf7510cc8dd22d
Saleem Toure <saleemtoure@gmail.com>
2024-06-29 23:19:28 +0200

    Update todo

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO SKILL UT KODEN I FLERE MER OVERSIKTLIGE KLASSER v1.1
         // TODO IMPROVE AESTHETHICS v2.0
         // TODO FORBEDRE TIDSFORDELING LOGIKKEN v2.1
-
         GUI gui = new GUI();
         gui.drawGUI();
     }
