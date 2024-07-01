d2398ab87260a2831252074400e61549d8ba7646
Saleem Toure <saleemtoure@gmail.com>
2024-07-01 20:34:22 +0200

    test 2

--- src/main/java/io/github/saleemtoure/Main.java
+++ src/main/java/io/github/saleemtoure/Main.java
@@ -1,9 +1,11 @@
 package io.github.saleemtoure;
+
 public class Main {
     public static void main(String[] args) {
         // TODO IMPROVE AESTHETHICS v2.0
         // TODO FORBEDRE TIDSFORDELING LOGIKKEN v2.1
         GUI gui = new GUI();
         gui.drawGUI();
+        //Test
     }
 }
