00a1586f739f446d25f104d12b969ac1d53a30a1
Saleem Toure Issifou <87614401+saleemtoure@users.noreply.github.com>
2024-07-02 19:13:18 +0200

    Removed testing variable

--- src/main/java/io/github/saleemtoure/MyCountDownTimer.java
+++ src/main/java/io/github/saleemtoure/MyCountDownTimer.java
@@ -30,8 +30,8 @@ class MyCountDownTimer implements ActionListener {
         timer = new Timer(1000, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (remainingTime > 0) {
-                    // remainingTime -= 1000;
-                    remainingTime -= 240000;
+                    remainingTime -= 1000;
+                    // remainingTime -= 240000; For testing
                     updateLabel();
                 } else {
                     timer.stop();
